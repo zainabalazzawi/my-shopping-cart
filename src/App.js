ee467e476b90fe6d9fca7b8d57947aedf0e81fbc
zainabalazzawi <zainabalazzawi91@gmail.com>
2021-07-29 17:51:48 +0300

    Add product component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+//import Products from "./components/products";
 
 function App() {
   return (
     <div className="main-container">
       <div className="content-container">
-        <div>fancy product</div>
+        <div className="header">Fancy products</div>
+        <span>(29)</span>
       </div>
+      {/*<Products />*/}
+
       <div className="cart-container">my cart </div>
     </div>
   );
