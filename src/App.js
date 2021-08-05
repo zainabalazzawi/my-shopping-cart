8b0f80ccfb8c536e0a13f89145303106a25ba244
alazzawimahmoud <alazzawi.mahmoud@gmail.com>
2021-08-05 19:54:31 +0200

    Explination

--- src/App.js
+++ src/App.js
@@ -2,18 +2,39 @@ import Products from "./components/Products/Products";
 import MyCart from "./components/MyCart/MyCart";
 import "./css/App.css";
 import useFetch from "./components/useFetch";
+import { useState } from "react";
 
 function App() {
   const { data } = useFetch("https://fakestoreapi.com/products");
+
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    console.log(product);
+    if (cart.find(cartItem => cartItem.id === product.id)) return;
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (product) => {
+    console.log(product);
+  };
+
   return (
     <div className="main-container">
       <div className="content-container">
         <div className="container-title">
           Fancy products <span className="container-list-total">(29)</span>
         </div>
-        <Products data={data} />
+
+        <Products
+          data={data}
+          addToCart={addToCart}
+          removeFromCart={addToCart}
+        />
+        
       </div>
-      <MyCart data={data} />
+
+      <MyCart cart={cart}   />
     </div>
   );
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,35 @@
 import AddToCart from "../AddToCart/AddToCart";
 import "./Products.css";
-const Products = ({ data }) => {
+const Products = ({ data, addToCart }) => {
   return (
     <div className="products">
       {data.map((product) => (
         <div className="product" key={product.id}>
-          <img className="img" src={product.image} alt={product.title} />
+          {/* With img element */}
+          {/* <div className="image-container">
+            <img className="image" src={product.image} alt={product.title} />
+          </div> */}
+
+          {/* As style backgroundImage */}
+          <div
+            className="image-container with-background-image"
+            title={product.title}
+            style={{ backgroundImage: `url(${product.image})` }}
+          ></div>
+
           <h1 className="price">${product.price}</h1>
           <p className="title">{product.title}</p>
           <span className="category">{product.category}</span>
-          <AddToCart product={product} />
+          
+          <button
+            type="button"
+            className="add-to-cart"
+            onClick={() => addToCart(product)}
+          >
+            Add to cart
+          </button>
+
+          {/* <AddToCart product={product} /> */}
         </div>
       ))}
     </div>
