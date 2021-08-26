229a8b3aed4d16f5b46337244d804e9ef95bb4fd
zainabalazzawi <zainabalazzawi91@gmail.com>
2021-08-26 09:36:28 +0300

    Add Height to product className

--- src/components/BackToStore/BackToStore.js
+++ src/components/BackToStore/BackToStore.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./BackToStore.css";
 
-const BackToStore = ({ setShowCart }) => {
+const BackToStore = () => {
   return (
     <div>
-      <button className="back-to-store" onClick={() => setShowCart(true)}>
-        Back to store
-      </button>
+      <button className="back-to-store">Back to store</button>
     </div>
   );
 };
